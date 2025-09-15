8a31b39d5315321b861151d08c1d1fc74585b82e
Marian Kamenistak <10233850+marian-kamenistak@users.noreply.github.com>
2025-09-15 14:24:26 +0200

    Update jira-work-item.ts

--- src/components/jira-work-item.ts
+++ src/components/jira-work-item.ts
@@ -1,6 +1,6 @@
 import { DurationIntervals, JiraExtractorConfig } from '../types';
 
-export const numTo2 = (s:number): string => s < 9 ? "0"+s : s.toString();
+export const numTo2 = (s:number): string => s <= 9 ? "0"+s : s.toString();
 
 export const dateToString = (date: Date): string => `${date.getUTCFullYear()}-${numTo2(date.getUTCMonth() + 1)}-${numTo2(date.getUTCDate())} ${numTo2(date.getUTCHours())}:${numTo2(date.getUTCMinutes())}:${numTo2(date.getSeconds())}`;
 
